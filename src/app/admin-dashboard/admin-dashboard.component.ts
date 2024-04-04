d4af66290da46e4a25d34f70bd2f404e2b64bffa
ala-kad <kaddechiala@gmail.com>
2024-04-04 23:43:31 +0100

    admin dashboard refactoring user listing into component and fixing style

--- src/app/admin-dashboard/admin-dashboard.component.ts
+++ src/app/admin-dashboard/admin-dashboard.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
 import { UserService } from '../services/user.service';
+import { UiInteractionsService } from '../services/ui-interactions.service';
+
+import { ModalComponent } from '../modal/modal.component';
+
 
 @Component({
   selector: 'app-admin-dashboard',
@@ -9,31 +13,62 @@ import { UserService } from '../services/user.service';
 })
 export class AdminDashboardComponent implements OnInit{
 
-  constructor(private userService: UserService) {} ;
+  constructor(private userService: UserService, private ui: UiInteractionsService) {} ;
 
   listOfData : any ;
   isCollapsed = false;
+  visible = false;
+  userDetails: any;
 
   ngOnInit(): void {
-    this.list();
+    // this.list();
   }
 
   /**
    * User listing
    */
-  list() {
-    this.userService.listUsers().subscribe(
+  // list() {
+  //   this.userService.listUsers().subscribe(
+  //     {
+  //       next: (data) =>{
+  //         this.listOfData = data;
+  //         console.log(data);
+  //         console.log(this.listOfData);
+  //       },
+  //       error: (err) =>{
+  //         console.log(err);
+  //       },
+  //     }
+  //   )
+  // }
+
+  showUser(id: any) {
+    this.userService.showUser(id).subscribe(
       {
-        next: (data) =>{
-          this.listOfData = data;
-          console.log(data);
-          console.log(this.listOfData);
+        next: (data) => {
+          this.userDetails = data;
+          console.log( this.userDetails );
+
         },
-        error: (err) =>{
+        error: (err) => {
           console.log(err);
-        },
+
+        }
       }
     )
+  }
+
+  open(id: any): void {
+    this.visible = true;
+    this.showUser(id);
+  }
+
+  close(): void {
+    this.visible = false;
+  }
+
+  openModal(userId: any) {
+    this.ui.openModal(userId);
   }
 
 }
