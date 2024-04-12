92e6c9fe8251d59ad0bb5006ae1fffb0eb96a08f
ala-kad <kaddechiala@gmail.com>
2024-04-12 09:22:21 +0100

    update user form component

---
+++ src/app/update-user-form/update-user-form.component.ts
@@ -0,0 +1,68 @@
+import { Component, OnInit } from '@angular/core';
+import { UserService } from '../services/user.service';
+import { ActivatedRoute, Router } from '@angular/router';
+
+@Component({
+  selector: 'app-update-user-form',
+  templateUrl: './update-user-form.component.html',
+  styleUrls: ['./update-user-form.component.css']
+})
+export class UpdateUserFormComponent implements OnInit {
+
+  user: any = {};
+  userId: any;
+
+  roles = [ {name: 'recruiter'},{name: 'rh'},{name: 'technique'}];
+
+  listOfOption: string[] = [];
+  listOfSelectedValue: string [] = [] ;
+
+  constructor(private userService : UserService, private activatedRoute: ActivatedRoute) {} ;
+
+  ngOnInit(): void {
+
+    const children: string[] = [];
+    this.roles.forEach((role) => {
+      children.push(role.name);
+    })
+    this.listOfOption = children;
+
+    this.activatedRoute.paramMap.subscribe(
+    (params) => {
+      this.userId = params.get('id');
+
+      this.userService.showUser(this.userId).subscribe(
+        {
+          next: (usr) => {
+            this.user = usr ;
+            this.userId = usr._id;
+            this.listOfSelectedValue = usr.role
+          }
+        }
+      )
+    },
+    (err) => {
+      console.log(err.message);
+    }
+    )
+
+
+  }
+
+   /** */
+   editUser(userId: any, selectedRoles: any[]) {
+    this.userService.editUser(userId, selectedRoles).subscribe(
+      {
+        next: (data) => {
+          console.log(data);
+
+        },
+        error: (err) => {
+          console.log(err);
+
+        }
+      }
+    )
+  }
+
+}
