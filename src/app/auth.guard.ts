c5c51a2bccbeacbef8b712fac8bb63ff935112a7
ala-kad <kaddechiala@gmail.com>
2024-04-12 15:38:57 +0100

    check if user logged in after page refresh

--- src/app/auth.guard.ts
+++ src/app/auth.guard.ts
@@ -1,14 +1,19 @@
-import { inject } from "@angular/core";
-import { Router } from "@angular/router";
+import { Injectable } from "@angular/core";
+import { CanActivate, CanActivateFn, Router } from "@angular/router";
 import { AuthService } from "./services/auth.service";
 
-export const AuthGuard = () => {
-    const auth = inject(AuthService);
-    const router = inject(Router);
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
 
-    if(!auth.isAuthenticatedFun()) {
-        router.navigateByUrl('/login')
-        return false
+  constructor(private authService: AuthService, private router: Router) {}
+  canActivate(): boolean {
+    if (this.authService.isAuthenticatedFun()) {
+      return true; // User is authenticated, allow access
+    } else {
+      this.router.navigateByUrl('/login');
+      return false; // User is not authenticated, deny access
     }
-    return true
+  }
 }
