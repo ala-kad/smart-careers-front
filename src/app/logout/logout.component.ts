117fa2bdc3e4c71114ce1213b92a903e53fcd890
ala-kad <kaddechiala@gmail.com>
2024-04-12 15:35:49 +0100

    logout component

---
+++ src/app/logout/logout.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-logout',
+  templateUrl: './logout.component.html',
+  styleUrls: ['./logout.component.css']
+})
+export class LogoutComponent {
+
+  constructor(
+    private router: Router
+  ) {}
+
+  logout() {
+    localStorage.removeItem('TOKEN');
+    this.router.navigateByUrl('login');
+  }
+}
