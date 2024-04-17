9410820512e5ae15d6bfb13e053f706c1a12e411
ala-kad <kaddechiala@gmail.com>
2024-04-17 16:22:11 +0100

    job details form component

---
+++ src/app/job-details-form/job-details-form.component.ts
@@ -0,0 +1,39 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { FormControl, FormGroup, FormBuilder, FormArray, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-job-details-form',
+  templateUrl: './job-details-form.component.html',
+  styleUrls: ['./job-details-form.component.css']
+})
+export class JobDetailsFormComponent implements OnInit{
+  @Input() startingForm: FormGroup | undefined;
+  @Output() subformInitialized: EventEmitter<FormGroup> = new EventEmitter<FormGroup>();
+  @Output() changeStep: EventEmitter<string> = new EventEmitter<string>();
+
+  constructor(
+    private fb: FormBuilder
+  ) {}
+
+  public jobDetailsForm: FormGroup | any ;
+
+  ngOnInit(): void {
+    if (this.startingForm) {
+      this.jobDetailsForm = this.startingForm;
+    } else {
+      this.jobDetailsForm = this.fb.group({
+        jobTitle: [''],
+        skills: [''],
+        requiredExperience: ['']
+      })
+    }
+   this.subformInitialized.emit(this.jobDetailsForm);
+
+  }
+
+
+  doChangeStep(direction: 'forward') {
+    this.changeStep.emit(direction);
+  }
+
+}
