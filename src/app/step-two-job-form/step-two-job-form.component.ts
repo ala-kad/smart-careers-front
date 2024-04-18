920d1d9e8b6649d763106519d2090eed4564de01
ala-kad <kaddechiala@gmail.com>
2024-04-18 17:00:34 +0100

    step two job form preselection questions

--- src/app/step-two-job-form/step-two-job-form.component.ts
+++ src/app/step-two-job-form/step-two-job-form.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
-
+import { Component,  OnInit } from '@angular/core';
+import { FormArray, FormBuilder,  } from '@angular/forms'
 @Component({
   selector: 'app-step-two-job-form',
   templateUrl: './step-two-job-form.component.html',
   styleUrls: ['./step-two-job-form.component.css']
 })
-export class StepTwoJobFormComponent {
 
+export class StepTwoJobFormComponent implements OnInit {
+
+  constructor (private _fb: FormBuilder) {} ;
+
+  stepTwoJobForm = this._fb.group({
+        jobDescription: [''],
+        profile: [''],
+        benefits: [''],
+      questions:
+        this._fb.array([this._fb.control('')])
+  })
+
+  ngOnInit(): void {
+
+  }
+
+
+  getQuestions () {
+    return this.stepTwoJobForm.get('questions') as FormArray;
+  }
+
+  addQuestion() {
+    this.getQuestions().push(this._fb.control(''));
+  }
+
+  removeQuestion (index: number) {
+    this.getQuestions().removeAt(index);
+  }
 }
