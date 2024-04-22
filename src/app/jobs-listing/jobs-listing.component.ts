6c5aad8d386aa28586f445cf14f2a505e99e2d7e
ala-kad <kaddechiala@gmail.com>
2024-04-22 15:29:39 +0100

    job listing component

--- src/app/jobs-listing/jobs-listing.component.ts
+++ src/app/jobs-listing/jobs-listing.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { JobsService } from '../services/jobs.service';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-jobs-listing',
   templateUrl: './jobs-listing.component.html',
   styleUrls: ['./jobs-listing.component.css']
 })
-export class JobsListingComponent {
+export class JobsListingComponent implements OnInit{
 
+  constructor (
+    private jobsService: JobsService,
+    private router: Router,
+    private activatedRoute: ActivatedRoute
+  ) {}
+
+  listOfJobs: any;
+  jobDetails: any ;
+
+  ngOnInit(): void{
+    this.jobsService.getJobsList().subscribe({
+      next: (data) => {
+       this.listOfJobs = data;
+       console.log(this.listOfJobs.length);
+
+      },
+      error: (err) => {
+        console.log(err.message);
+
+      }
+    })
+  }
+
+  navigateToJobDetails(id: any) {
+    this.router.navigate(['./', id], { relativeTo: this.activatedRoute});
+  }
 }
