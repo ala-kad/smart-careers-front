b346348cf33fa27dfd02b02d7d678e255135d475
ala-kad <kaddechiala@gmail.com>
2024-04-25 08:58:26 +0100

    publish offer, get job list job service

--- src/app/services/jobs.service.ts
+++ src/app/services/jobs.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 @Injectable({
@@ -11,17 +11,23 @@ export class JobsService {
     private http: HttpClient
   ) { }
 
-  getJobsList():Observable<any> {
-    return this.http.get('http://localhost:3000/jobs/');
+  getJobsList(status: string): Observable<any> {
+    let params = new HttpParams().set('status', status)
+    console.log(status);
+
+    return this.http.get('http://localhost:3000/jobs/', { params: params });
   }
 
-
-  getJobDetails(id: Number):Observable<any> {
+  getJobDetails(id: Number): Observable<any> {
     return this.http.get(`http://localhost:3000/jobs/${id}`);
   }
 
   deleteJob(id: any): Observable<any> {
     return this.http.delete(`http://localhost:3000/jobs/${id}`);
+  }
+
+  publishJobOffer(id: any, {}): Observable<any> {
+    return this.http.patch(`http://localhost:3000/jobs/publish/${id}`, {});
   }
 
 }
