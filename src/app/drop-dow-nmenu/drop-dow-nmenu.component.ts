25532ed95da4f1aef31e6d8e4f3fb83882c28ea3
ala-kad <kaddechiala@gmail.com>
2024-04-29 16:38:09 +0100

    drop down menu logout

---
+++ src/app/drop-dow-nmenu/drop-dow-nmenu.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-drop-dow-nmenu',
+  templateUrl: './drop-dow-nmenu.component.html',
+  styleUrls: ['./drop-dow-nmenu.component.css']
+})
+export class DropDowNmenuComponent {
+
+}
