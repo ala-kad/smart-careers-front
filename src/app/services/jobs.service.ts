7aacd9cf90ae99affce2b895bd71e895ca280781
ala-kad <kaddechiala@gmail.com>
2024-04-30 11:25:08 +0100

    Post new Job Offer service

--- src/app/services/jobs.service.ts
+++ src/app/services/jobs.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpParams } from '@angular/common/http';
+import { HttpClient, HttpParams, HttpRequest } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 @Injectable({
@@ -13,8 +13,6 @@ export class JobsService {
 
   getJobsList(status: string): Observable<any> {
     let params = new HttpParams().set('status', status)
-    console.log(status);
-
     return this.http.get('http://localhost:3000/jobs/', { params: params });
   }
 
@@ -28,6 +26,11 @@ export class JobsService {
 
   publishJobOffer(id: any, {}): Observable<any> {
     return this.http.patch(`http://localhost:3000/jobs/publish/${id}`, {});
+  }
+
+  postNewJobOffer (body: any): Observable<any> {
+    console.log(body)
+    return this.http.post(`http://localhost:3000/jobs/`, body);
   }
 
 }
