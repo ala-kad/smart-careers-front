c3f6a243426ed6462a60b849888196d5417f6ab5
ala-kad <kaddechiala@gmail.com>
2024-05-10 23:29:18 +0100

    Job Details + Job Listing components within Home Module

---
+++ src/app/home/job-listing/job-listing.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-job-listing',
+  templateUrl: './job-listing.component.html',
+  styleUrls: ['./job-listing.component.css']
+})
+export class JobListingComponent {
+
+}
