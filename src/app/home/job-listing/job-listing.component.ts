13c746cc3bc5126dffdcb996c2ee23f9a90796e9
ala-kad <kaddechiala@gmail.com>
2024-05-12 23:15:08 +0100

    Job Listing Component within Home module

--- src/app/home/job-listing/job-listing.component.ts
+++ src/app/home/job-listing/job-listing.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { JobsService } from '../../services/jobs.service';
 
 @Component({
   selector: 'app-job-listing',
   templateUrl: './job-listing.component.html',
   styleUrls: ['./job-listing.component.css']
 })
-export class JobListingComponent {
+export class JobListingComponent implements OnInit{
 
+  jobList: any;
+  status = 'Published';
+
+  constructor(
+    private jobService: JobsService
+  ) {}
+
+  ngOnInit(): void {
+    this.getPublishedJobs()
+  }
+
+
+  getPublishedJobs() {
+    this.jobService.getJobsList(this.status).subscribe({
+      next: (data) => {
+        this.jobList = data;
+      },
+      error: (err) => {
+        console.log(err);
+      }
+    })
+  }
 }
