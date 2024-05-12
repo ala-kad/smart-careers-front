bb5d65ec33cbc13367983663cae258ddde0d0f25
ala-kad <kaddechiala@gmail.com>
2024-05-12 23:14:36 +0100

    Job Details TS within Home Module

--- src/app/home/job-details/job-details.component.ts
+++ src/app/home/job-details/job-details.component.ts
@@ -1,10 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { JobsService } from 'src/app/services/jobs.service';
 
 @Component({
   selector: 'app-job-details',
   templateUrl: './job-details.component.html',
   styleUrls: ['./job-details.component.css']
 })
-export class JobDetailsComponent {
+export class JobDetailsComponent implements OnInit {
+
+  jobId!: any;
+  jobDetails!: any;
+
+  constructor (
+    private activatedRoute: ActivatedRoute,
+    private jobService: JobsService
+  ) {}
+
+  ngOnInit(): void {
+    this.activatedRoute.paramMap.subscribe({
+      next: (params) => {
+        this.jobId = params.get("id");
+      },
+       error: (err) =>{
+        console.log(err);
+
+      }
+    })
+    this.fetchJobDetails();
+  }
+
+  fetchJobDetails() {
+    this.jobService.getJobDetails(this.jobId).subscribe({
+      next: (data) => {
+        this.jobDetails = data;
+        console.log(data);
+
+      },
+      error: (err) => {
+        console.log(err);
+      }
+    })
+  }
 
 }
