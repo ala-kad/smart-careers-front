256f5408561225d1ba46f625a86104d58c047700
ala-kad <kaddechiala@gmail.com>
2024-05-12 23:13:11 +0100

    Importing Component and Modules

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { HomeRoutingModule } from './home-routing.module';
+import { NgZorroAntdModulesModule } from '../shared/modules/ng-zoro-antd-modules/ng-zorro-antd-modules.module';
+import { IconsProviderModule } from '../shared/modules/ng-zoro-antd-modules/icons-provider.module';
+
+
 import { HomeComponent } from './home.component';
 import { JobListingComponent } from './job-listing/job-listing.component';
 import { JobDetailsComponent } from './job-details/job-details.component';
@@ -15,7 +18,9 @@ import { JobDetailsComponent } from './job-details/job-details.component';
   ],
   imports: [
     CommonModule,
-    HomeRoutingModule
+    HomeRoutingModule,
+    NgZorroAntdModulesModule,
+    IconsProviderModule
   ]
 })
 export class HomeModule { }
