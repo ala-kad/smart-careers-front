b92b5a73d08f2f7fc9973c2a232f5b7d834475c3
ala-kad <kaddechiala@gmail.com>
2024-05-12 23:11:48 +0100

    Home Routing Module

--- src/app/home/home-routing.module.ts
+++ src/app/home/home-routing.module.ts
@@ -1,11 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home.component';
+import { JobListingComponent } from './job-listing/job-listing.component'
+import { JobDetailsComponent } from './job-details/job-details.component';
 
 const routes: Routes = [
   {
-    path: '', component: HomeComponent,
-  }
+    path: '',
+    component: HomeComponent,
+    children: [
+      {
+        path: '',
+        component: JobListingComponent
+      },
+      {
+        path: 'jobs/:id',
+        component: JobDetailsComponent
+      }
+    ]
+  },
 ];
 
 @NgModule({
