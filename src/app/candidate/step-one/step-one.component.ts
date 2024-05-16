341556ce8d38c74c02ee80391f239b7f668e8ec3
ala-kad <kaddechiala@gmail.com>
2024-05-16 10:30:55 +0100

    Styling, validating step one job application form within candidate module

--- src/app/candidate/step-one/step-one.component.ts
+++ src/app/candidate/step-one/step-one.component.ts
@@ -1,34 +1,111 @@
-import { Component, OnInit } from '@angular/core';
-import {FormGroup, FormBuilder, FormArray, Validators} from '@angular/forms';
+import { Component, OnInit, EventEmitter, Output, OnChanges, SimpleChanges } from '@angular/core';
+import {FormGroup, FormBuilder, FormArray, Validators, FormControl} from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+import { AuthService } from 'src/app/services/auth.service';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-step-one',
   templateUrl: './step-one.component.html',
   styleUrls: ['./step-one.component.css']
 })
+export class StepOneComponent implements OnInit, OnChanges {
 
-export class StepOneComponent implements OnInit {
-
+  @Output() formEmitter: EventEmitter<FormGroup> = new EventEmitter<FormGroup>();
   formInfos!: FormGroup;
+  candidateInfos!: any;
+  candidateId: any;
 
   constructor (
     private fb: FormBuilder,
-  ){
+    private authService: AuthService,
+    private userService: UserService,
+    private activatedRoute: ActivatedRoute,
+  ) 
+  { 
     this.formInfos = this.fb.group({
-      avatar: ['', [Validators.required]],
+      avatar: ['',],
       firstname: ['', [Validators.required]],
       lastname: ['', [Validators.required]],
-      email: ['', [Validators.required]],
-      confirmEmail: ['', [Validators.required]],
+      email: ['', [Validators.required, Validators.email]],
+      confirmEmail: ['', [Validators.required, Validators.email, this.confirmationValidator]],
       adress: ['', [Validators.required]],
       phone: ['', [Validators.required]],
-      phoneNumberPrefix: [''],
+      phoneNumberPrefix: ['+216'],
+      cv: ['', [Validators.required]]
     })
   }
 
-  ngOnInit(): void {
-
+  ngOnChanges(changes: SimpleChanges): void {
+    if(changes['formEmitter']) { 
+      console.log('changed');
+      
+    }
   }
 
+  ngOnInit(): void {    
+    this.formEmitter.emit(this.formInfos)
+    this.activatedRoute.paramMap.subscribe(
+      (params) => {
+        this.candidateId = params.get('userId'); 
+        this.fetchCandidateInfos();
+      }
+    )
+  }
 
+  confirmationValidator = (control: FormControl): { [s: string]: boolean } => {
+    if (!control.value) {
+      return { required: true };
+    } else if (control.value !== this.formInfos.controls['email'].value) {
+      return { confirm: true, error: true };
+    }
+    return {};
+  };
+
+  updateConfirmValidator(): void {
+    /** wait for refresh value */
+    Promise.resolve().then(() => this.formInfos.controls['confirmEmail'].updateValueAndValidity());
+  }
+
+  fetchCandidateInfos() {
+    this.userService.showUser(this.candidateId).subscribe({
+      next: (data) => {
+        this.candidateInfos = data
+        this.initForm();
+      },
+      error: (err) => {
+        console.log(err);
+
+      }
+    })
+  }
+
+  initForm() {
+    this.formInfos = this.fb.group({
+      avatar: ['',],
+      firstname: [this.candidateInfos.firstname, [Validators.required]],
+      lastname: [this.candidateInfos.lastname, [Validators.required]],
+      email: [this.authService.getUserCrendentials().email , [Validators.required, Validators.email]],
+      confirmEmail: ['', [Validators.required, Validators.email, this.confirmationValidator]],
+      adress: [this.candidateInfos.adress, [Validators.required]],
+      phone: [this.candidateInfos.phone, [Validators.required]],
+      phoneNumberPrefix: ['+216'],
+      cv: ['', [Validators.required]]
+    })
+    this.formEmitter.emit(this.formInfos);
+  }
+
+  submitForm() { 
+    if(this.formInfos.valid) {
+      console.log('valid');
+    }
+    else {
+      Object.values(this.formInfos.controls).forEach(control => {
+        if (control.invalid) {
+          control.markAsDirty();
+          control.updateValueAndValidity({ onlySelf: true });
+        }
+      });
+    }
+  }
 }
