5b8e514996f59e4cb06ceda2591a84c21508a91b
ala-kad <kaddechiala@gmail.com>
2024-05-16 11:01:15 +0100

    Styling application Form Template

--- src/app/candidate/application-form/application-form.component.ts
+++ src/app/candidate/application-form/application-form.component.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+import { UiInteractionsService } from 'src/app/services/ui-interactions.service';
 
 @Component({
   selector: 'app-application-form',
   templateUrl: './application-form.component.html',
   styleUrls: ['./application-form.component.css']
 })
-export class ApplicationFormComponent {
+export class ApplicationFormComponent implements OnInit {
+
   current = 0;
+  stepOneForm!: FormGroup;
+
+  constructor (
+    private ui: UiInteractionsService ,
+  ) { }
+
+  ngOnInit(): void {
+  }
 
   pre(): void {
     this.current -= 1;
@@ -14,10 +25,42 @@ export class ApplicationFormComponent {
 
   next(): void {
     this.current += 1;
+    switch (this.current) {
+      case 0:
+        console.log(this.current);
+        
+      break;
+
+      case 1:
+        console.log(this.current);
+
+      break;
+
+      case 2:
+        console.log(this.current);
+        
+      break;
+
+      case 3:
+        console.log(this.current);
+        
+      break;
+
+      default:
+        console.log(this.current);
+
+      break;
+    }
   }
 
   done() {
-    console.log('done');
+    this.ui.openSuccessJobModal()
+  }
+
+  recieveStepOneForm(form: FormGroup) {
+    this.stepOneForm = form;
+    console.log(this.stepOneForm);
+    
   }
 
 }
