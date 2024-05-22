937dc56171d789647d32557d4ae1981c8094afca
ala-kad <kaddechiala@gmail.com>
2024-05-22 09:27:00 +0100

    application form component

--- src/app/candidate/application-form/application-form.component.ts
+++ src/app/candidate/application-form/application-form.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup } from '@angular/forms';
+import { ApplicationService } from 'src/app/services/application.service';
 import { UiInteractionsService } from 'src/app/services/ui-interactions.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-application-form',
@@ -10,13 +12,35 @@ import { UiInteractionsService } from 'src/app/services/ui-interactions.service'
 export class ApplicationFormComponent implements OnInit {
 
   current = 0;
-  stepOneForm!: FormGroup;
+  candidateId: any;
+  jobId: any;
+
+  stepOneFormData: FormData = new FormData();
+  responsesStepTWo!: any[];
+
+  applicationFormValues: any;
+
+  stepOneFormValue: any ;
 
   constructor (
     private ui: UiInteractionsService ,
+    private applicationService: ApplicationService,
+    private activatedRoute: ActivatedRoute,
   ) { }
 
   ngOnInit(): void {
+    this.activatedRoute.paramMap.subscribe({
+      next: (params) => {
+        this.candidateId = params.get('userId');
+        this.jobId = params.get('jobId');
+      },
+      }
+    )
+  }
+
+  recieveStepOneFormValue(value: String) {
+    this.stepOneFormValue = value;
+    console.log(this.stepOneFormValue);
   }
 
   pre(): void {
@@ -53,14 +77,40 @@ export class ApplicationFormComponent implements OnInit {
     }
   }
 
+  recieveStepOneForm(form: FormData) {
+    this.stepOneFormData = form;
+    console.log(this.stepOneFormData);
+  }
+
+  receiveStepTwoResponses(responses: string[]){
+    this.responsesStepTWo = responses;
+  }
+
   done() {
-    this.ui.openSuccessJobModal()
+    this.applicationFormValues =  {...this.stepOneFormValue, ...this.responsesStepTWo}
+    this.applicationService.sendJobApplication(this.stepOneFormData, this.applicationFormValues, this.candidateId, this.jobId).subscribe({
+      next: (res) =>{ 
+        this.ui.openSuccessJobModal()
+        console.log('Values String Input', this.applicationFormValues);
+        console.log('Form Data', this.stepOneFormData)
+        
+      },
+      error: (err) => { 
+        console.log(err);
+      }
+    })
+
+    // this.applicationService.sendApplication(this.stepOneFormData, '5', this.candidateId, this.jobId).subscribe({
+    //   next: (data) => { 
+    //     console.log(this.stepOneFormData);
+    //     console.log(data);
+    //   },
+    //   error: (err) => { 
+    //     console.log(err);
+        
+    //   }
+    // })
   }
 
-  recieveStepOneForm(form: FormGroup) {
-    this.stepOneForm = form;
-    console.log(this.stepOneForm);
-    
-  }
-
+ 
 }
